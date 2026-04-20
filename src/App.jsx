ef759f0e3b7c83a49b2aaa0cd78183f12b0f33d8
Ibrahim Khawaja <ibrahimmkhawaja@example.com>
2026-04-20 23:15:45 +0500

    new

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Hero from "./Hero.jsx";
 import MenuPage from "./MenuPage.jsx";
 import Navbar from "./components/Navbar.jsx";
-import AboutUs from "./Aboutus.jsx";
+import Aboutus from "./Aboutus.jsx";
 import ContactUs from "./ContactUs.jsx";
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Hero />} />
         <Route path="/menu" element={<MenuPage />} />
-        <Route path="/about" element={<AboutUs />} />
+        <Route path="/about" element={<Aboutus />} />
         <Route path="/contact" element={<ContactUs />} />
       </Routes>
     </Router>
